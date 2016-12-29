d95b2da735976b1135618a367de9c18424b26461
Shichengyao <shichengyao@le.com>
2016-12-29 16:17:14 +0800

    add LogiTick

---
+++ src/main/java/com/scy/service/ToutiaoService.java
@@ -0,0 +1,13 @@
+package com.scy.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Shichengyao on 12/12/16.
+ */
+@Service
+public class ToutiaoService {
+    public String say(){
+        return "This is from ToutiaoService";
+    }
+}
