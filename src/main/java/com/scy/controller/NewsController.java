f1c7e5347445affc7f3e77265cab4eca0afc2847
Shichengyao <scyangle1314@163.com>
2017-01-09 22:04:00 +0800

    add  Image

--- src/main/java/com/scy/controller/NewsController.java
+++ src/main/java/com/scy/controller/NewsController.java
@@ -6,12 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -23,6 +27,17 @@ public class NewsController {
     private static final Logger logger = LoggerFactory.getLogger(NewsController.class);
     @Autowired
     private NewsService newsService;
+
+    @RequestMapping(value = "/image",method = RequestMethod.GET)
+    @ResponseBody
+    public void getImage(@RequestParam("name") String imageName, HttpServletResponse response) {
+        try {
+            response.setContentType("image/jpeg");
+            StreamUtils.copy(new FileInputStream(new File(ToutiaoUtils.IMAGE_DIR+imageName)), response.getOutputStream());
+        } catch (IOException e) {
+            logger.error("读取图片错误" + imageName, e);
+        }
+    }
 
     @RequestMapping(path = {"/uploadImage"}, method = {RequestMethod.POST})
     @ResponseBody
