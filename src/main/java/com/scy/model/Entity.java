7be872657927d6a26c3e4895e1bd9ee443978fbb
Shichengyao <scyangle1314@163.com>
2017-01-12 21:04:25 +0800

    Comment Center

---
+++ src/main/java/com/scy/model/Entity.java
@@ -0,0 +1,8 @@
+package com.scy.model;
+
+/**
+ * Created by Shichengyao on 1/12/17.
+ */
+public class Entity {
+    public static final int ENTITY_NEWS = 1;
+}
---
+++ src/main/java/com/scy/service/CommentService.java
@@ -0,0 +1,7 @@
+package com.scy.service;
+
+/**
+ * Created by Shichengyao on 1/12/17.
+ */
+public class CommentService {
+}
