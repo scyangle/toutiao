66955da39a205cfc675a5a9a03beeecfa51f5ae6
Shichengyao <scyangle1314@163.com>
2017-01-24 11:07:06 +0800

    change properties for the autowire

--- src/main/java/com/scy/service/QiniuService.java
+++ src/main/java/com/scy/service/QiniuService.java
@@ -10,6 +10,7 @@ import com.qiniu.util.Auth;
 import com.scy.utils.ToutiaoUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
@@ -22,23 +23,25 @@ import java.util.UUID;
  * Created by Shichengyao on 1/10/17.
  */
 @Service
-public class QiniuService {
+public class QiniuService implements InitializingBean{
     private static final Logger logger = LoggerFactory.getLogger(QiniuService.class);
     //设置好账号的ACCESS_KEY和SECRET_KEY
-    String ACCESS_KEY="Gv9AxSxtA85iMTkOw98k16WBMz7HvSflqBQAw_8h";
-    String SECRET_KEY="fZ2KAqUZaNewiwx34J4u5_B5ZThw8kDvh_YyHrmK";
+    @Value("${qiniu.AccessKey}")
+    private  String ACCESS_KEY;
+    @Value("${qiniu.SecretKey}")
+    private  String SECRET_KEY;
     //要上传的空间
-    String bucketname = "image";
-    private static String QINIU_IMAGE_DOMAIN = "http://ojkbh6krj.bkt.clouddn.com/";
+    @Value("${qiniu.bucketname}")
+    private  String bucketname;
+    @Value("${qiniu.imageDomain}")
+    private  String QINIU_IMAGE_DOMAIN;
     //密钥配置
-    Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
+    Auth auth =null;
     //第二种方式: 自动识别要上传的空间(bucket)的存储区域是华东、华北、华南。
-    Zone z = Zone.autoZone();
-    Configuration c = new Configuration(z);
-
+    Zone z =null;
+    Configuration c=null;
     //创建上传对象
-    UploadManager uploadManager = new UploadManager(c);
-
+    UploadManager uploadManager=null;
     //简单上传，使用默认策略，只需要设置上传的空间名就可以了
     public String getUpToken() {
         return auth.uploadToken(bucketname);
@@ -69,23 +72,12 @@ public class QiniuService {
         }
     }
 
-//    public void upload() throws IOException {
-//        try {
-//            //调用put方法上传
-//            Response res = uploadManager.put(FilePath, key, getUpToken());
-//            //打印返回的信息
-//            System.out.println(res.bodyString());
-//        } catch (QiniuException e) {
-//            Response r = e.response;
-//            // 请求失败时打印的异常的信息
-//            System.out.println(r.toString());
-//            try {
-//                //响应的文本信息
-//                System.out.println(r.bodyString());
-//            } catch (QiniuException e1) {
-//                //ignore
-//            }
-//        }
-//    }
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        auth = Auth.create(ACCESS_KEY, SECRET_KEY);
+        z = Zone.autoZone();
+        c = new Configuration(z);
+        uploadManager = new UploadManager(c);
+    }
 }
 
