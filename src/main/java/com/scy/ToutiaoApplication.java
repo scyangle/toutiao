887783d7cd597349abe9ea20cf8a6064ac7e4c7e
Scyangle <scyangle1314@163.com>
2017-06-11 13:05:01 +0800

    update

--- src/main/java/com/scy/ToutiaoApplication.java
+++ src/main/java/com/scy/ToutiaoApplication.java
@@ -1,5 +1,4 @@
 package com.scy;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
